35ddc66348270252d8d611e2f413e90711b6633e
Abdallah Hesham <124697660+AbdallahHesham44@users.noreply.github.com>
2024-12-18 13:57:45 +0200

    Create deployToKubernetes.groovy

---
+++ shared-library/vars/deployToKubernetes.groovy
@@ -0,0 +1,13 @@
+// vars/deployToKubernetes.groovy
+
+def call(String namespace) {
+    // Use credentials to fetch the Kubernetes kubeconfig
+    withCredentials([file(credentialsId: 'aks', variable: 'KUBECONFIG')]) {
+        // Deploy to Kubernetes using the provided namespace
+        sh """
+            export KUBECONFIG=${KUBECONFIG}
+            kubectl apply -f deployment.yaml --namespace=${namespace}
+            kubectl get pods --namespace=${namespace}
+        """
+    }
+}
